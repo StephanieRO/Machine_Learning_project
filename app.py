7e8b449205734251b15c80b6ac686cfa0753b01f
StephanieRO <86803264+StephanieRO@users.noreply.github.com>
2025-01-29 11:20:37 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -17,10 +17,10 @@ st.title('Predicción de si el cliente pertenese al cluster 1() o 0()')
 job = st.selectbox('Trabajo', ['profesional','t_manuales','out_laboral','unknown'], index=None, placeholder="seleccione una opcion")
 marital = st.selectbox('Estado civil',['single','married','divorced'], index=None, placeholder="seleccione una opcion" )
 education = st.selectbox('Educacion', ['primary','secondary','tertiary','unknown'], index=None, placeholder="seleccione una opcion")
-default = st.number_input('Balance de cuenta del cliente (euros)', min_value=0, max_value=1)
+default = st.number_input('Default (1 = si, 0 = no)', min_value=0, max_value=1)
 balance = st.number_input('Balance de cuenta del cliente (euros)', min_value=0)
-housing = st.number_input('Balance de cuenta del cliente (euros)', min_value=0, max_value=1)
-loan = st.number_input('Balance de cuenta del cliente (euros)', min_value=0, max_value=1)
+housing = st.number_input('Housing (1 = si, 0 = no)', min_value=0, max_value=1)
+loan = st.number_input('Housing (1 = si, 0 = no)', min_value=0, max_value=1)
 
 # Crear un DataFrame con las entradas
 user_data = pd.DataFrame({
