c1cf29610a2c0dd8504e2ccc74726f430d8aa45a
StephanieRO <86803264+StephanieRO@users.noreply.github.com>
2025-01-30 09:43:23 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ with open('kmeans_model.pkl', 'rb') as model_file:
 st.title('Predicción de si el cliente pertenese al cluster 1(No deposito) o 0(Si deposito)')
 
 # Entrada de datos del usuario
-job = st.selectbox('Trabajo (seleccione una opcion)', ['profesional','t_manuales','out_laboral','unknown'])
+job = st.selectbox('Trabajo (seleccione una opcion)', ['blue-collar','entrepreneur','housemaid','management','retired', 'self-employed', 'services', 'student', 'technician', 'unemployed', 'unknown'])
 marital = st.selectbox('Estado civil (seleccione una opcion)',['single','married','divorced'])
 education = st.selectbox('Educacion (seleccione una opcion)', ['primary','secondary','tertiary','unknown'])
 default = st.number_input('Default (1 = si, 0 = no)', min_value=0, max_value=1)
