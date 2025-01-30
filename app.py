13eee3cfc7e6b9568738a8b3a96745a0c669097a
StephanieRO <86803264+StephanieRO@users.noreply.github.com>
2025-01-30 10:19:24 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -17,14 +17,11 @@ st.title('Clasificación de Clientes Bancarios')
 job = st.selectbox('Trabajo (seleccione una opcion)', ['blue-collar','entrepreneur','housemaid','management','retired', 'self-employed', 'services', 'student', 'technician', 'unemployed', 'unknown'])
 marital = st.selectbox('Estado civil (seleccione una opcion)',['single','married','divorced'])
 education = st.selectbox('Educacion (seleccione una opcion)', ['primary','secondary','tertiary','unknown'])
-default = st.radio('Default', [0, 1], captions=[
-        "No",
-        "Si"
-    ],)
-#default = st.number_input('Default (1 = si, 0 = no)', min_value=0, max_value=1)
+default = st.radio('Default', [0, 1], captions=["No","Si"], horizontal=True)
 balance = st.number_input('Balance de cuenta del cliente (euros)', min_value=0)
-housing = st.number_input('Housing (1 = si, 0 = no)', min_value=0)
-loan = st.number_input('loan (1 = si, 0 = no)', min_value=0)
+housing = st.radio('Housing', [0, 1], captions=["No","Si"], horizontal=True)
+loan = st.radio('loan', [0, 1], captions=["No","Si"], horizontal=True)
+
 
 # Crear un DataFrame con las entradas
 user_data = pd.DataFrame({
@@ -43,8 +40,10 @@ user_data_standardized = loaded_preprocessor.transform(user_data)
 # Realizar la predicción
 prediction_cluster = loaded_kmeans.predict(user_data_standardized)
 
+
+st.write("Resultado del Clustering: ")
 # Mostrar la predicción
 if prediction_cluster[0] == 0:
-    st.write("El cliente pertenece al Cluster 0: Clientes con mayor probabilidad de realizar depósitos.")
+    st.write("El cliente pertenece al Cluster 0: mayor probabilidad de realizar depósitos.")
 else:
-    st.write("El cliente pertenece al Cluster 1: Clientes con menor probabilidad de realizar depósitos.")
+    st.write("El cliente pertenece al Cluster 1: menor probabilidad de realizar depósitos.")
